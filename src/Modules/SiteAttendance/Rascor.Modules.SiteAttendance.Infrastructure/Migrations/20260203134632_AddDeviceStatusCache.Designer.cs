cdb88235de69ab82f749f1c000655076768fb637
UncleMarvo <eddie_heff@yahoo.com>
2026-02-03 13:51:25 +0000

    fix: model snapshot was out of sync

--- src/Modules/SiteAttendance/Rascor.Modules.SiteAttendance.Infrastructure/Migrations/20260203134632_AddDeviceStatusCache.Designer.cs
+++ src/Modules/SiteAttendance/Rascor.Modules.SiteAttendance.Infrastructure/Migrations/20260203134632_AddDeviceStatusCache.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Rascor.Modules.SiteAttendance.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Rascor.Modules.SiteAttendance.Infrastructure.Persistence;
 namespace Rascor.Modules.SiteAttendance.Infrastructure.Migrations
 {
     [DbContext(typeof(SiteAttendanceDbContext))]
-    partial class SiteAttendanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260203134632_AddDeviceStatusCache")]
+    partial class AddDeviceStatusCache
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
